bd13d7292d482f7c517ce8d4ad72d8f4e189c638
Kharman Bradley <kharmanbradley03@gmail.com>
2024-02-19 12:49:18 +0000

    Bar draw function added Allows for easily drawing a line that crosses the width of the terminal no matter the starting size of terminal

--- FIFO-LIFO-Sim.sh
+++ FIFO-LIFO-Sim.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env sh
+
+barDraw(){
+	barCounter=1
+	termWidth=$(stty size | cut -d " " -f 2)
+	while [ $barCounter -lt $termWidth ]; do
+		printf "%0.s─" $barCounter
+		barCounter=$(( $barCounter+1 ))
+	done
+}
+
+barDraw
+echo ""
