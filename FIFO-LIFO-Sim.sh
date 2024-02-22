1cf06a54a55f35f12baae5f1fd6ce26359b57bef
KMBradley <kharmanbradley03@gmail.com>
2024-02-22 22:44:57 +0000

    Finished menu
    Menu is now functioning; accepts user input and compares to options
    Menu option validation tied to logged in account
    Added custom exit function that can both be called, and hijacks sigint to prompt for confirmation

--- FIFO-LIFO-Sim.sh
+++ FIFO-LIFO-Sim.sh
@@ -12,6 +12,27 @@ cyan="\e[36m"
 default="\e[0m"
 #printf colour https://stackoverflow.com/a/5412776
 
+#Setup an exit handler function
+confirmQuit() {
+	trap - SIGINT	#Reset sigint to normal behaviour
+	echo -ne "\nAre you sure you wish to exit? [y/n]: "; read -r leave
+	if [ "$leave" = "y" ]; then
+		clear
+		centerText "Goodbye $username" "R" "$green" "$green"
+		sleep 2
+		clear
+		exit
+	else
+		echo "Shortly resuming program from prior prompt..."
+		trap "confirmQuit" INT
+		sleep 2
+		return
+	fi
+}
+
+#Override SIGINT to do our bidding; https://stackoverflow.com/a/14702379
+trap "confirmQuit" INT
+
 #Called with Type, Optionally colour
 barDraw(){
 	barCounter=1
@@ -86,25 +107,91 @@ centerText(){
 	echo -e "\033[0m"	#Reset to term default colour
 }
 
-barDraw "T" $cyan
-centerText "Hewwo!" "M" $cyan
-barDraw "J" $cyan
-centerText "" "M" $cyan
-centerText "1)   Login    " "M" $cyan $purple
-centerText "2)  FIFO Sim  " "M" $cyan $purple
-centerText "3)  LIFO Sim  " "M" $cyan $purple
-if [ "$username" == "Admin" ]; then
-	centerText "4) Pass change" "M" $cyan $purple
-	centerText "" "M" $cyan
+#Called with nothing
+drawMenu(){
+	barDraw "T" $cyan
+	centerText "Hewwo!" "M" $cyan $purple
 	barDraw "J" $cyan
 	centerText "" "M" $cyan
-	centerText "5)   Admin    " "M" $cyan $purple
-elif [ "$username" != "" ]; then
-	centerText "4) Pass change" "M" $cyan $purple
-fi
-centerText "" "M" $cyan
-barDraw "J" $cyan
-centerText "Exit" "M" $cyan $red
-barDraw "B" $cyan
+	centerText "1)   Login    " "M" $cyan $purple
+	centerText "2)  FIFO Sim  " "M" $cyan $purple
+	centerText "3)  LIFO Sim  " "M" $cyan $purple
+	if [ "$username" == "Admin" ]; then
+		centerText "4) Pass change" "M" $cyan $purple
+		centerText "" "M" $cyan
+		barDraw "J" $cyan
+		centerText "" "M" $cyan
+		centerText "5)   Admin    " "M" $cyan $purple
+	elif [ "$username" != "" ]; then
+		centerText "4) Pass change" "M" $cyan $purple
+	fi
+	centerText "" "M" $cyan
+	barDraw "J" $cyan
+	centerText "Exit" "M" $cyan $red
+	barDraw "B" $cyan
+}
 
-echo "$(stty size | cut -d " " -f 2)"
+#Menu options
+loginHandler(){
+	echo "This will run the login code"
+}
+callFIFO(){
+	if [ "$username" == "" ]; then
+		echo "Please login before trying to run a simulation"
+		return
+	fi
+	echo "This will run the FIFO simulation"
+}
+callLIFO(){
+	if [ "$username" == "" ]; then
+		echo "Please login before trying to run a simulation"
+		return
+	fi
+	echo "This will run the FIFO simulation"
+}
+passChangeHandler(){
+	if [ "$username" != "" ]; then
+		echo "This will run the password change code only if user is logged in"
+	fi
+}
+adminStuffs(){
+	if [ "$username" == "Admin" ]; then
+		echo "This will run the admin utilites if the current user is logged in as admin"
+	fi
+}
+
+#Debug output
+echo "Term width is: $(stty size | cut -d " " -f 2)"
+echo "Term height is: $(stty size | cut -d " " -f 1)"
+
+#Program loop
+while true; do
+	drawMenu
+	echo -ne '\nEnter an option: '; read -r menuChoice
+
+	if [ "$menuChoice" == "1" ]; then
+		loginHandler
+	elif [ "$menuChoice" == "2" ]; then
+		callFIFO
+	elif [ "$menuChoice" == "3" ]; then
+		callLIFO
+	elif [ "$menuChoice" == "4" ]; then
+		if [ "$username" != "" ]; then
+			passChangeHandler
+		else
+			echo "Please enter a valid option from the menu, or enter Bye to exit at any time"
+		fi
+	elif [ "$menuChoice" == "5" ]; then
+		if [ "$username" == "Admin" ]; then
+			adminStuffs
+		else
+			echo "Please enter a valid option from the menu, or enter Bye to exit at any time"
+		fi
+	elif [ "$menuChoice" == "Exit" ] || [ "$menuChoice" == "Bye" ]; then
+		confirmQuit
+	else
+		echo "Please enter a valid option from the menu, or enter Bye to exit at any time"
+	fi
+	sleep 2
+	clear
+done
