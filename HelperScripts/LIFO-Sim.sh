86f43185765a14ed6b36cc7b322f2bc4d6666caa
Kharman Bradley <kharmanbradley03@gmail.com>
2024-04-06 16:03:12 +0100

    Added queue generation
    Both FIFO and LIFO sims generate a queue correctly
    Queue is stored in a variable for now, required to be in a file before submission

--- HelperScripts/LIFO-Sim.sh
+++ HelperScripts/LIFO-Sim.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env sh
 
 echo "LIFO Addon File called!"
+
+genQueue(){
+	if [ $# -eq 0 ]; then
+		count=10
+	else
+		count=$1
+	fi
+	byteNo=0
+	rawQueue=$(shuf -i0-99 -n $count);
+	cleanedQueue="";
+	while IFS= read -r byte; do
+		byteNo=$(( byteNo+1 ))
+		if [ "${#byte}" -eq 1 ] && [ "$byteNo" -ne "$count" ]; then
+			cleanedQueue+="B0$byte,"
+		elif [ "${#byte}" -eq 2 ] && [ "$byteNo" -ne "$count" ]; then
+			cleanedQueue+="B$byte,"
+		elif [ "${#byte}" -eq 1 ]; then
+			cleanedQueue+="B0$byte"
+		elif [ "${#byte}" -eq 2 ]; then
+			cleanedQueue+="B$byte"
+		fi
+	done <<< "$rawQueue";
+	echo "$cleanedQueue"
+}
+
+genQueue 50
