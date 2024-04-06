5a792bcefdf96a8ef483c4589fbb0a8db29c8025
Kharman Bradley <kharmanbradley03@gmail.com>
2024-04-06 17:13:52 +0100

    Redone queue generation
    After committing I realised shuf was not available in TinyCore (has no coreutils)
    
    New solution uses dd, one of the original ideas I had but thought too complex
    Additional manual collision checking added
    Queue is now correctlty written to a "simdata_username.job" file
    Added *.job to .gitignore

--- HelperScripts/LIFO-Sim.sh
+++ HelperScripts/LIFO-Sim.sh
@@ -9,21 +9,45 @@ genQueue(){
 		count=$1
 	fi
 	byteNo=0
-	rawQueue=$(shuf -i0-99 -n $count);
-	cleanedQueue="";
-	while IFS= read -r byte; do
-		byteNo=$(( byteNo+1 ))
-		if [ "${#byte}" -eq 1 ] && [ "$byteNo" -ne "$count" ]; then
-			cleanedQueue+="B0$byte,"
-		elif [ "${#byte}" -eq 2 ] && [ "$byteNo" -ne "$count" ]; then
-			cleanedQueue+="B$byte,"
-		elif [ "${#byte}" -eq 1 ]; then
-			cleanedQueue+="B0$byte"
+
+	while [ "$byteNo" -lt "$count" ]; do
+		#Generate the byte
+		byte=$(head /dev/urandom | od -An -N1 -d)	#https://linuxsimply.com/bash-scripting-tutorial/operator/arithmetic-operators/random-number/
+		byte=$(( $byte % 100 ))						#% 100 was decent advise from a friend for how to constrain the output of od
+
+		#0 pad if needed
+		if [ "${#byte}" -eq 1 ]; then
+			cleanedByte="B0$byte"
 		elif [ "${#byte}" -eq 2 ]; then
-			cleanedQueue+="B$byte"
+			cleanedByte="B$byte"
 		fi
-	done <<< "$rawQueue";
-	echo "$cleanedQueue"
+
+		#B00 is invalid, so change to B01
+		if [ "$cleanedByte" = "B00" ]; then
+			cleanedByte="B01"
+		fi
+
+		#Collision check and write
+		#Check if file doesn't exist (it shouldn't) then make that new file
+		if [ ! -f "simdata_$username.job" ]; then
+			byteNo=$(( $byteNo+1 ))		#Increment by 1 as always ran
+			echo -n "$cleanedByte," > "simdata_$username.job"
+		else
+			#Check if byte is already written
+			if [ "$(grep -c $cleanedByte simdata_$username.job)" -eq 0 ]; then
+				byteNo=$(( $byteNo+1 ))		#Increment by 1 as no collision
+				if [ "$byteNo" -lt "$count" ]; then
+					echo -n "$cleanedByte," >> "simdata_$username.job"
+				else
+					echo "$cleanedByte" >> "simdata_$username.job"
+				fi
+			else
+				echo "Byte collision, regenning byte $(( $byteNo+1 ))"
+			fi
+		fi
+	done
+	return 0
 }
 
-genQueue 50
+rm "simdata_$username.job"
+genQueue 10
